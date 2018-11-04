6e3721017eeefc6879060b0f007b9e8a3ed44dd4
chenjianing <chenjianing@360.cn>
2018-11-04 13:05:01 +0800

    大改后第一次提交。calc改为根据操作符优先级计算，并增加四则运算

---
+++ mini_ruler/calc.py
@@ -0,0 +1,190 @@
+# -*- coding:utf-8 -*-
+
+
+calc_tbl = {
+    '!': lambda x, y=None: not x,
+    '~=': lambda x, y: x in y,
+
+    '&&': lambda x, y: x and y,
+    '||': lambda x, y: x or y,
+
+    '>': lambda x, y: x > y,
+    '>=': lambda x, y: x >= y,
+    '<': lambda x, y: x < y,
+    '<=': lambda x, y: x <= y,
+    '==': lambda x, y: x == y,
+    '!=': lambda x, y: x != y,
+
+    '*': lambda x, y: x * y,
+    '/': lambda x, y: x / y,
+    '+': lambda x, y: x + y,
+    '-': lambda x, y: x - y,
+}
+
+def baisc_calc(op, x, y=None):
+    result = calc_tbl[op](x, y)
+    return result
+
+def get_token_value(env, tok):
+    if tok[0] == 'ID':
+        value = env.get_var(tok[1])
+    elif tok[0] == 'CALL':
+        call, args = tok[1][0], tok[1][1]
+        call = env.get_var(call)
+        new_args = [ get_token_value(env, arg) for arg in args ]
+        value = call(*new_args)
+    else:
+        value = tok[1]
+    return value
+
+
+def create_token(x):
+    if type(x) == int: return ('INTEGER', x)
+    if type(x) == float: return ('FLOAT', x)
+    if type(x) == str: return ('STRING', x)
+    if type(x) == bool:  return ('BOOL', x)
+    # if type(x) == type(None):  return ('None', None)
+    raise Exception('Unknow token type %s' % type(x))
+
+
+def token_calc(env, op, x, y=None):
+    op = op[1]
+    x = get_token_value(env, x)
+    y = get_token_value(env, y)
+
+    result = baisc_calc(op, x, y)
+
+    print "Result !!!!!!!!!! %s" % str(result)
+
+    result = create_token(result)
+
+    raise Exception("result error type: %s" % type(result))
+
+class OperatorError(Exception):
+    def __init__(self, err):
+        Exception.__init__(self, err)
+
+
+class Operator():
+    op_tbl = [
+        # 优先级低
+        ['='],
+        ['||'],
+        ['&&'],
+        ['==', '!=', '~='],
+        ['>', '>=', '<', '<='],
+        ['+', '-'],
+        ['*', '/'],
+        ['!'],
+        ['.'],
+        ['call', 'item'],
+        # 优先级高
+    ]
+
+    op_level = {}
+    for i, one_level in enumerate(op_tbl):
+        for op in one_level:
+            op_level[op] = i
+
+    @classmethod
+    def level(cls, op):
+        if not op in cls.op_level:
+            raise OperatorError("operator `%s` does not support" % op)
+        return cls.op_level[op]
+
+
+class CalcStack:
+
+    def __init__(self):
+        self.stack = [([], [])]
+        self.operands = self.stack[-1][0]
+        self.operators = self.stack[-1][1]
+
+    def push(self):
+        self.stack.append(([], []))
+        self.operands = self.stack[-1][0]
+        self.operators = self.stack[-1][1]
+
+    def pop(self):
+        operands, operators = self.stack.pop()
+        self.operands = self.stack[-1][0]
+        self.operators = self.stack[-1][1]
+        return operands, operators
+
+def calc(env, tokens):
+
+    result = []
+
+    stack = CalcStack()
+
+    def calc_stack_top():
+        print "calc_stack_top"
+        optr = stack.operators.pop()
+        oprd2, oprd1 = stack.operands.pop(), stack.operands.pop()
+
+        # print optr, oprd1, oprd2
+        result = token_calc(env, optr, oprd1, oprd2)
+        stack.operands.append(result)
+
+    def flush_out_stack():
+        print "flush_out_stack"
+        while len(stack.operators):
+            print("optr: " + str(stack.operators))
+            print("oprd: " + str(stack.operands))
+            calc_stack_top()
+            print("optr: " + str(stack.operators))
+            print("oprd: " + str(stack.operands))
+
+        if len(stack.operands) > 1:
+            raise Exception('Stack is not balance')
+        result.append(stack.operands[0])
+
+
+    tok_iter = iter(tokens)
+
+    tok = tok_iter.next()
+    while True:
+        try:
+            print tok
+
+            if tok[0] in ['INTEGER', 'FLOAT', 'STRING', 'BOOL']:
+                stack.operands.append(tok)
+                tok = tok_iter.next()
+            if tok[0] in ['ID', 'CALL']:
+                stack.operands.append(create_token(get_token_value(env, tok)))
+                tok = tok_iter.next()
+            elif tok[0] in ['LOGICAL_OPERATOR', 'RELATIONAL_OPERATOR', 'ARITHMEITC_OPERATOR']:
+                if not len(stack.operators):
+                    stack.operators.append(tok)
+                    tok = tok_iter.next()
+                else:
+                    if Operator.level(stack.operators[-1][1]) < Operator.level(tok[1]):
+                        stack.operators.append(tok)
+                        tok = tok_iter.next()
+                    else:
+                        calc_stack_top()
+            elif tok[0] == 'LPAREN':
+                stack.push()
+                tok = tok_iter.next()
+            elif tok[0] == 'RPAREN':
+                flush_out_stack()
+                operands, _ = stack.pop()
+                stack.operands.append(operands)
+                tok = tok_iter.next()
+            elif tok[0] == 'COMMA':
+                flush_out_stack()
+                break
+            else:
+                raise Exception('Unknow tok %s' % str(tok))
+        except(StopIteration):
+            flush_out_stack()
+            break
+        finally:
+            # print("")
+            # print("optr: " + str(stack.operators) )
+            # print("oprd: " + str(stack.operands) )
+            pass
+
+
+    print
+    return stack.operands[0]
--- mini_ruler/lexer.py
+++ mini_ruler/lexer.py
@@ -18,12 +18,12 @@ class RuleLexer:
 
     states = (
         ('func', 'exclusive'),
-        ('paren', 'exclusive'),
     )
 
     tokens = (
         'LOGICAL_OPERATOR',
         'RELATIONAL_OPERATOR',
+        'ARITHMEITC_OPERATOR',
 
         'FUNCSTART',
         'FUNCSTOP',
@@ -31,23 +31,43 @@ class RuleLexer:
         'LPAREN',
         'RPAREN',
 
-        'VAR',
-        'NUMBER',
+        # 'COMMA',
+
+        'ID',
+
+        'INTEGER',
         'STRING',
+        'BOOL',
+        # 'NULL',
     )
 
-    t_INITIAL_paren_LOGICAL_OPERATOR      = r'&&|\|\|'
-    t_INITIAL_paren_RELATIONAL_OPERATOR   = r'==|\!=|>=*|<=*'
+    t_ANY_LOGICAL_OPERATOR      = r'&&|\|\|'
+    t_ANY_RELATIONAL_OPERATOR   = r'==|\!=|>=*|<=*|~='
+    t_ANY_ARITHMEITC_OPERATOR   = r'\+|-|\*|/'
 
-    t_ANY_VAR               = r'[a-zA-Z_@][0-9a-zA-Z._-]*'
+    t_ANY_LPAREN  = r'\('
+    t_ANY_RPAREN  = r'\)'
 
-    def t_ANY_NUMBER(self, t):
-        r'\d+(.\d+)?'
-        t.value =  float(t.value) if '.' in t.value else int(t.value)
+    # t_func_COMMA = r','
+
+
+    def t_ANY_BOOL(self, t):
+        r'\b(TRUE|FALSE)\b'
+        t.value = True if t.value == "TRUE" else False
+        return t
+
+    # def t_ANY_NULL(self, t):
+    #     r'\bNULL\b'
+    #     t.value = None
+    #     return t
+
+    def t_ANY_INTEGER(self, t):
+        r'\d+'
+        t.value = int(t.value)
         return t
 
     def t_ANY_STRING(self, t):
-        r'\'([^\'\\]|\\\'|\\)*\''
+        r'\"[^\"]*\"'
         t.value = t.value[1:-1]
         return t
 
@@ -64,17 +84,27 @@ class RuleLexer:
         t.type = 'FUNCSTOP'
         return t
 
-    def t_ANY_begin_paren(self, t):
-        r'\('
-        t.lexer.push_state('paren')
-        t.type = 'LPAREN'
+    def t_ANY_ID(self, t):
+        r'[a-zA-Z_@][\w_-]*(\.[a-zA-Z_@][\w_-]*)*'
+        # r'[a-zA-Z_][\w_-]*'
         return t
 
-    def t_paren_end(self, t):
-        r'\)'
-        t.lexer.pop_state()
-        t.type = 'RPAREN'
-        return t
+    def parse_call(self, start_tok):
+        try:
+            args = []
+            for tok in self.lexer:
+                tok = (tok.type, tok.value)
+                if tok[0] == 'FUNCSTOP':
+                    break
+                elif tok[0] == 'FUNCSTART':
+                    tok = self.parse_call()
+                args.append(tok)
+        except StopIteration:
+            raise Exception('Lost FUNCSTOP')
+
+        tok = ('CALL', (start_tok[1], tuple(args)))
+        return tok
+
 
     def parse_toekns(self, data):
         try:
@@ -84,37 +114,13 @@ class RuleLexer:
                 # DEBUG PRINT
                 # print(self.lexer.lexstatestack, self.lexer.lexstate, (tok.type, tok.value))
                 tok = (tok.type, tok.value)
+                print tok
 
-                if tok[0] == 'FUNCSTOP':
-                    func_name, args = None, []
-                    while len(lex_tokens):
-                        history_tok = lex_tokens.pop()
-                        if history_tok[0] == 'FUNCSTART':
-                            func_name = history_tok[1]
-                            break
-                        args.insert(0, history_tok)
-                    if not func_name:
-                        raise Exception("Can not find FUNCSTART")
-                    tok = ('FUNC', (func_name, tuple(args)))
-                elif tok[0] == 'RPAREN':
-                    args = []
-                    while len(lex_tokens):
-                        history_tok = lex_tokens.pop()
-                        if history_tok[0] == 'LPAREN':
-                            break
-                        args.insert(0, history_tok)
-                    tok = ('PAREN', tuple(args))
+                if tok[0] == 'FUNCSTART':
+                    tok = self.parse_call(tok)
 
                 lex_tokens.append(tok)
 
-                if len(lex_tokens) >= 3 \
-                    and lex_tokens[-3][0] in ['VAR', 'NUMBER', 'STRING', 'FUNC'] \
-                    and lex_tokens[-2][0] in ['RELATIONAL_OPERATOR'] \
-                    and lex_tokens[-1][0] in ['VAR', 'NUMBER', 'STRING', 'FUNC'] \
-                    :
-                    oprd2, optr, oprd1 = lex_tokens.pop(), lex_tokens.pop(), lex_tokens.pop()
-                    tok = ('PAREN', (oprd1, optr, oprd2))
-                    lex_tokens.append(tok)
         except(lex.LexError) as e:
             print("Error: %s" % e)
             print(data)
@@ -124,3 +130,41 @@ class RuleLexer:
 
     def __init__(self, **kwargs):
         self.lexer = lex.lex(module=self, **kwargs)
+
+
+
+if __name__ == "__main__":
+
+    from pprint import pprint
+
+    from calc import calc
+
+    from ruler_env import RulerEnv
+
+    from basic_action import re_match
+
+    lexer = RuleLexer()
+
+    env = RulerEnv()
+
+    env.set_var('re_match', re_match)
+
+
+
+    # data = 'a.b == "test" + "str" && 1 + 2047 == 2048 '
+    data = 're_match("\d", "1")'
+
+    tokens = lexer.parse_toekns(data)
+
+
+    # print
+    # pprint(tokens)
+    # print
+
+    env.push()
+
+    env.set_var('a', {'b': 'teststr'})
+
+    print calc(env, tokens)
+
+    env.pop()
