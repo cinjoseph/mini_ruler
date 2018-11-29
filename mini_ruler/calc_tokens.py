e1a3de2b34e55eeda9e416e78e4e7bd88079519b
chenjianing <chenjianing@360.cn>
2018-11-29 15:20:02 +0800

    1、增加not运算 2、在最终运算时再算ID和call的值

--- mini_ruler/calc_tokens.py
+++ mini_ruler/calc_tokens.py
@@ -1,8 +1,18 @@
 # -*- coding:utf-8 -*-
 
 
+def calc_not_operate(x):
+    if type(x) == bool:
+        return not x
+    if type(x) in [int, float]:
+        return not x
+    if x is None:
+        return True
+    return False
+
+
 calc_tbl = {
-    '!': lambda x, y=None: not x,
+    '!': lambda x, y=None: calc_not_operate(x),
     '~=': lambda x, y: x in y,
 
     '&&': lambda x, y: x and y,
@@ -44,24 +54,30 @@ def create_token(x):
     if type(x) == float: return ('FLOAT', x)
     if type(x) == str: return ('STRING', x)
     if type(x) == bool:  return ('BOOL', x)
-    
+    if type(x) == type(None): return ('NULL', x)
+    if type(x) == type(dict): return ('DICT', x)
+    if type(x) == type(list): return ('LIST', x)
+    if type(x) == type(tuple): return ('TUPLE', x)
+
     # 目前还不支持 None, list, dict, tple 等类型, 做特殊处理
-    # 当获取到None时， 为False
-    if type(x) == type(None):  return ('BOOL', False)
-    # 为了只有一个id变量的时候，获取到这些的空值导致判断结果为False
-    if type(x) in [dict, list, tuple]:  return ('BOOL', True)
+    # # 当获取到None时， 为False
+    # if type(x) == type(None):  return ('BOOL', False)
+    # # 为了只有一个id变量的时候，获取到这些的空值导致判断结果为False
+    # if type(x) in [dict, list, tuple]:  return ('BOOL', True)
     raise Exception('Unknow token type %s %s' % (type(x), x))
 
 
-def token_calc(env, op, x, y=None):
+def token_calc(env, op, xtok, ytok=None):
     op = op[1]
-    x = get_token_value(env, x)
-    y = get_token_value(env, y) if y else None
+    x = get_token_value(env, xtok)
+    # print("Get %s value=%s" % (xtok, x))
+    y = get_token_value(env, ytok) if ytok else None
+    # print("Get %s value=%s" % (ytok, y))
 
     result = baisc_calc(op, x, y)
-
     result = create_token(result)
     return result
+
 
 class OperatorError(Exception):
     def __init__(self, err):
@@ -103,6 +119,42 @@ class CalcStack:
         self.operands = self.stack[-1][0]
         self.operators = self.stack[-1][1]
 
+    def operators_stack_top(self):
+        if len(self.operators) > 0:
+            return self.operators[-1]
+        return None
+
+    def push_operator(self, tok):
+        self.operators.append(tok)
+        # print("\npush operator %s" % str(tok))
+        # print("optr: " + str(self.operators))
+        # print("oprd: " + str(self.operands))
+
+    def pop_operator(self):
+        tok = self.operators.pop()
+        # print("\nPop operator %s" % str(tok))
+        # print("optr: " + str(self.operators))
+        # print("oprd: " + str(self.operands))
+        return tok
+
+    def operands_stack_top(self):
+        if len(self.operands) > 0:
+            return self.operands[-1]
+        return None
+
+    def push_operand(self, tok):
+        self.operands.append(tok)
+        # print("\npush operand %s" % str(tok))
+        # print("optr: " + str(self.operators))
+        # print("oprd: " + str(self.operands))
+
+    def pop_operand(self):
+        tok = self.operands.pop()
+        # print("\nPop operand %s" % str(tok))
+        # print("optr: " + str(self.operators))
+        # print("oprd: " + str(self.operands))
+        return tok
+
     def push(self):
         self.stack.append(([], []))
         self.operands = self.stack[-1][0]
@@ -122,24 +174,19 @@ def calc(env, tokens):
 
     def calc_stack_top():
         # print "calc_stack_top"
-        optr = stack.operators.pop()
+        optr = stack.pop_operator()
         if optr[0] in ['NOT_OPERATOR']: # 单目运算符
-            oprd2, oprd1 = None, stack.operands.pop()
+            oprd2, oprd1 = None, stack.pop_operand()
         else: # 双目运算符
-            oprd2, oprd1 = stack.operands.pop(), stack.operands.pop()
+            oprd2, oprd1 = stack.pop_operand(), stack.pop_operand()
         result = token_calc(env, optr, oprd1, oprd2)
-        stack.operands.append(result)
+        # print("Calc %s %s %s = %s" % (oprd1, optr, oprd2, result))
+        stack.push_operand(result)
 
     def flush_out_stack():
-        # print "flush_out_stack"
-        while len(stack.operators):
-            # print("optr: " + str(stack.operators))
-            # print("oprd: " + str(stack.operands))
+        while stack.operators_stack_top():
             calc_stack_top()
-            # print("optr: " + str(stack.operators))
-            # print("oprd: " + str(stack.operands))
-
-        if len(stack.operands) > 1:
+        if stack.operators_stack_top():
             raise Exception('Stack is not balance')
         result.append(stack.operands[0])
 
@@ -151,21 +198,23 @@ def calc(env, tokens):
             # print tok
 
             if tok[0] in ['INTEGER', 'FLOAT', 'STRING', 'BOOL', 'NULL']:
-                stack.operands.append(tok)
+                stack.push_operand(tok)
                 tok = tok_iter.next()
             elif tok[0] in ['ID', 'CALL']:
-                stack.operands.append(create_token(get_token_value(env, tok)))
+                stack.push_operand(tok)
+                # stack.push_operand(create_token(get_token_value(env, tok)))
                 tok = tok_iter.next()
             elif tok[0] in ['LOGICAL_OPERATOR', 'RELATIONAL_OPERATOR', 'ARITHMEITC_OPERATOR', 'NOT_OPERATOR']:
-                if not len(stack.operators):
-                    stack.operators.append(tok)
-                    tok = tok_iter.next()
-                else:
+                stack_top = stack.operators_stack_top()
+                if stack_top:
                     if Operator.level(stack.operators[-1][1]) < Operator.level(tok[1]):
-                        stack.operators.append(tok)
+                        stack.push_operator(tok)
                         tok = tok_iter.next()
                     else:
                         calc_stack_top()
+                else:
+                    stack.push_operator(tok)
+                    tok = tok_iter.next()
             elif tok[0] == 'LPAREN':
                 stack.push()
                 tok = tok_iter.next()
@@ -179,10 +228,9 @@ def calc(env, tokens):
         except(StopIteration):
             flush_out_stack()
             break
-        finally:
-            # print("")
-            # print("optr: " + str(stack.operators) )
-            # print("oprd: " + str(stack.operands) )
-            pass
 
-    return stack.operands[0][1]
+
+    # print stack.operands_stack_top()
+    result = get_token_value(env, stack.operands_stack_top())
+    # print result
+    return result
